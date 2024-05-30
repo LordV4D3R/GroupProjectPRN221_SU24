cef10180e3e4e5f9e42469865c2e43fb5305295d
ThinhNguyen2304 <vippro2304@gmail.com>
2024-05-30 16:18:47 +0700

    add entity

---
+++ MSA.Domain/Entities/Feedback.cs
@@ -0,0 +1,15 @@
+﻿using MSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA.Domain.Entities
+{
+    public class Feedback : BaseEntity
+    {
+        public string Context {  get; set; } = string.Empty;
+        public string img_Url { get; set; } = string.Empty;
+    }
+}
