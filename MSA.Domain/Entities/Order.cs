e4d244708022841cc26380a05fb6058595fffa24
tranan <tranquocan1508@gmail.com>
2024-05-30 22:17:58 +0700

    [+] Seed Data, commit change from My Laptop to continued my work on my Pc

--- MSA.Domain/Entities/Order.cs
+++ MSA.Domain/Entities/Order.cs
@@ -1,16 +1,21 @@
 ﻿using MSA.Domain.Common;
+using MSA.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MSA.Domain.Entities
 {
+    [Table("order")]
     public class Order : BaseEntity
     {
+        [Column("total_price")]
         public double TotalPrice {  get; set; }
         public double VoucherId { get; set; }
-        public int Status { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public OrderRefundStatus OrderRefundStatus { get; set; }
     }
 }
