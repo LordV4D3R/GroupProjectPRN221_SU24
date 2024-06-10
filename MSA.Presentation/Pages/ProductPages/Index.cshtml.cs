07c397ac6d592807948659da8e91d08989522fa8
ThinhNguyen2304 <vippro2304@gmail.com>
2024-06-10 20:17:38 +0700

    Seed data Category + Product

---
+++ MSA.Presentation/Pages/ProductPages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MSA.Domain.Entities;
+using MSA.Infrastructure;
+
+namespace MSA.Presentation.Pages.ProductPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MSA.Infrastructure.ApplicationDbContext _context;
+
+        public IndexModel(MSA.Infrastructure.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products
+                .Include(p => p.Category).ToListAsync();
+        }
+    }
+}
