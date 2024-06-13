1d97735d24cb3807101498bb2a080c35f987ad96
tranan <tranquocan1508@gmail.com>
2024-06-13 21:23:24 +0700

    [+] Add Html css Template

--- MSA.Presentation/Program.cs
+++ MSA.Presentation/Program.cs
@@ -1,17 +1,46 @@
 using Microsoft.EntityFrameworkCore;
+using MSA.Application.IRepositories;
+using MSA.Application.IServices;
+using MSA.Application.Services;
 using MSA.Infrastructure;
+using MSA.Infrastructure.Repositories;
+using MSA.Infrastructure.Services;
 using MSA.Presentation.SeedData;
+using Repositories;
+using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
 
+// Add Repository to the container.
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IBatchRepository, BatchRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IVoucherRepository, VoucherRepository>();
+
+//Add Services
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IBatchService, BatchService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFeedbackService, FeedbackService>();
+builder.Services.AddScoped<IOrderDetailService, OrderDetailService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IVoucherService, VoucherService>();
+
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
