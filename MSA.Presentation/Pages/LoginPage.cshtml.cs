f07b6b327e38b942bdb6daf8238ba0093e1fa6b2
Vader <tranquocan1508@gmail.com>
2024-06-17 12:54:54 +0700

    [+] Add template int wwwroot, change template in index.cshtml

---
+++ MSA.Presentation/Pages/LoginPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSA.Presentation.Pages
+{
+    public class LoginPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
