4fb196b6f88cb9ff97c287e6dbd672bb863bc33b
Vader <tranquocan1508@gmail.com>
2024-06-19 21:04:36 +0700

    [+] Add Product Dto

---
+++ MSA.Domain/Dtos/Product/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MSA.Domain.Common;
+using MSA.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA.Domain.Dtos.Product
+{
+    public class ProductViewModel : BaseEntity
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public double Price { get; set; } = 0;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public int TotalQuantity { get; set; } = 0;
+        public ProductStatus Status { get; set; } = ProductStatus.ComingSoon;
+
+    }
+}
--- MSA.Presentation/Pages/Index.cshtml.cs
+++ MSA.Presentation/Pages/Index.cshtml.cs
@@ -1,20 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MSA.Domain.Dtos.Product;
+using Services;
 
 namespace MSA.Presentation.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IProductService _productService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
+        }
+        [BindProperty]
+        public ProductViewModel ProductViewModel { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            return Page();
         }
 
-        public void OnGet()
+        private void LoadData()
         {
-
+            var product = _productService.GetAll;
+            ProductViewModel = new ProductViewModel
+            {
+                ProductName = "Product 1",
+                Price = 100000,
+                Description = "Description 1",
+                ImageUrl = "https://via.placeholder.com/150",
+                TotalQuantity = 10
+            };
         }
     }
 }
