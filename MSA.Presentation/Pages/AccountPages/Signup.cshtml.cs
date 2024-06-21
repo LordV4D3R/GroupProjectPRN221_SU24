b3e3a2f5cf40843bf70351155429ed52b411bf1c
ThinhNguyen2304 <vippro2304@gmail.com>
2024-06-21 15:23:15 +0700

    Add signup

---
+++ MSA.Presentation/Pages/AccountPages/Signup.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MSA.Application.IServices;
+using MSA.Domain.Account;
+using MSA.Domain.Dtos.Account;
+using MSA.Domain.Entities;
+using MSA.Domain.Enums;
+using MSA.Infrastructure;
+
+namespace MSA.Presentation.Pages.AccountPages
+{
+    public class SignupModel : PageModel
+    {
+        private readonly IAccountService _accountService;
+        public SignupModel(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Account Account { get; set; } = default!;
+        public SignUpViewModel request { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync(SignUpViewModel request)
+        {
+            Account newAccount = new Account
+            {
+                Username = request.Username,
+                Password = request.Password,
+                FullName = request.FullName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                Address = request.Address,
+                ImageUrl = request.ImageUrl,
+                Role = RoleType.Customer,
+                Status = AccountStatus.Active,
+            };
+            _accountService.Add(newAccount);
+            _accountService.Save();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
