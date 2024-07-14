adbd0ff58636fddea4cea28cbad340a039d0e457
Vader <tranquocan1508@gmail.com>
2024-07-14 17:51:26 +0700

    [+] Fix Layout, Add OrderPage for user

---
+++ MSA.Presentation/Pages/GuestPages/OrderManage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSA.Presentation.Pages.GuestPages
+{
+    public class OrderManageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
